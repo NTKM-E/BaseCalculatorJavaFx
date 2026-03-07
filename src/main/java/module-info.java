558185a76107be499ce09f61267da973707d29aa
Andy Vega <asvc2005@hotmail.com>
2026-03-06 16:33:15 -0800

    Added, base word docx, added apache dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module nt.basecalulator.basecalculator {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.poi.ooxml;
 
 
     opens nt.basecalulator.basecalculator to javafx.fxml;
